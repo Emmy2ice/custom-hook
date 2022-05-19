ee6b3894fdda14ef55d1eb9e9a06abd760c41c95
Chukwuemeka <eimuoh@gmail.com>
2022-05-19 06:04:45 +0100

    Create a React app and publish it to Githup Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import UseFetch from './UseFetch';
+
 
 function App() {
+  const BASE_URL = "https://reqres.in/api/users/";
+  const { data: users, loading, error } = UseFetch(BASE_URL);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+     <h1>Custom React Hook (Data fetching)</h1>
+     {loading && <h3>loading...</h3>}
+     {error && <h3>Oops! Something went wrong</h3>}
+     <div>
+       <pre>{ JSON.stringify(users, undefined, 2)}</pre>
+     </div>
     </div>
   );
 }
